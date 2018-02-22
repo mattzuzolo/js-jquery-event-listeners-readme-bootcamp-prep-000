a31bf9809d97045c7c0e82f2fcc555ea3c299c85
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-22 02:45:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -33,7 +33,7 @@ function submitIt () {
   $("form").on("submit", function () {
    
     if ($ ("input:first" ).val() === "correct") {
-      alert ("Your form is going to be submitted now.");
+      alert ('Your form is going to be submitted now.');
       
       return;
     } //if statement ends here
