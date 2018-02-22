77975858d7f6386da011f2692be9d46f5c19816c
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-22 02:48:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,48 @@
 //define functions here
 
+function getIt () {
+  
+  $('p').on("click", function() {
+    
+      alert("Hey!");    
+    
+  }); //close parentheses here for jquery thing
+}
+
+function frameIt () {
+  $('img').on('load', function() {
+    $("img").addClass("tasty"); //look up .addClass() on jQuery website for more info.
+    
+  }); //close parentheses here for jquery thing
+  
+}
+    
+function pressIt () {
+  
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("You pressed 'G'");
+      
+    }
+  }); //close parentheses here for jquery thing
+  
+}
+
+function submitIt () {
+  
+  $("form").on("submit", function () {
+      alert ('Your form is going to be submitted now.');
+      return;
+  }); //close parentheses here for jquery thing
+  
+  
+  
+}
+
+
+
+
+
 $(document).ready(function(){
 
 // call functions here
