7f3081bb94e05d36289484ca3b879719d39612d9
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-22 02:31:17 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function getIt () {
 
 function frameIt () {
   $('#tasty').on('load', function() {
-    $("div img").addClass("tasty");
+    $("img").addClass("tasty");
     
   }); //close parentheses here for jquery thing
   
